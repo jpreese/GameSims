da1180d6ea082cf221985081824c62d3c41c9825
John Reese <jpreese@mtu.edu>
2017-07-10 15:31:12 -0400

    Initial commit

---
+++ TicTacToe/TicTacToe/Models/Board.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace TicTacToe.Models
+{
+    public class Board
+    {
+        private const int BOARD_SIZE = 9;
+        public readonly Space[] Spaces;
+
+        public Board()
+        {
+            Spaces = new Space[BOARD_SIZE];
+
+            for (int x = 0; x < BOARD_SIZE; x++)
+            {
+                Spaces[x] = new Space(x);
+            }
+        }
+
+        public bool IsFull()
+        {
+            return GetFreeSpaces().Count == 0;
+        }
+
+        public List<Space> GetFreeSpaces()
+        {
+            var freeSpaces = new List<Space>();
+
+            for (int x = 0; x < BOARD_SIZE; x++)
+            {
+                if(Spaces[x].OccupiedBy == OccupationType.Empty)
+                {
+                    freeSpaces.Add(Spaces[x]);
+                }
+            }
+
+            return freeSpaces;
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Models/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TicTacToe.Models
+{
+    public class Player
+    {
+        public readonly OccupationType Occupation;
+        public List<Space> Moves;
+
+        public Player(OccupationType occupation)
+        {
+            Occupation = occupation;
+            Moves = new List<Space>();
+        }
+
+        public void TakeTurn(Board board)
+        {
+            var space = FindRandomFreeSpace(board);
+            space.OccupiedBy = Occupation;
+
+            Console.WriteLine(string.Format("{0} has chosen {1}", Occupation, space.Location));
+        }
+
+        private Space FindRandomFreeSpace(Board board)
+        {
+            var freeSpaces = board.GetFreeSpaces();
+
+            var random = new Random();
+            var randomSpaceIndex = random.Next(0, freeSpaces.Count);
+
+            return freeSpaces.ElementAt(randomSpaceIndex);
+        }
+    }
+}
